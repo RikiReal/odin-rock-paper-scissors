6e51d6f36ba8b7a47c1bae5e2e73a34bc02129b8
RikiReal <rijad@tutanota.de>
2024-10-11 15:43:58 +0200

    Implement getComputerChoice() function
    Add a function that returns a random choice of rock, paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    // Math.random * 3 ==> 0 - 2.99999, 
+    // Math.floor ==> 0 - 2
+    // +1 => 1 - 3
+    const choices = ["rock", "paper", "scissors"];
+    const choiceIndex = Math.floor(Math.random() * 3);
+    return choices[choiceIndex];
+}
+
+console.log(getComputerChoice())
