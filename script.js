e99a7f3311d8ef8a57aadde0ea1d1207ea200142
RikiReal <rijad@tutanota.de>
2024-10-11 16:25:00 +0200

    Implement game loop
    Implement the game loop. Play 5 rounds of rock-paper-scissors. The one with the most points wins

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const validChoices = ["rock", "paper", "scissors"];
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice() {
   // Math.random * 3 ==> 0 - 2.99999,
@@ -51,6 +49,53 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+      if (computerChoice === "scissors") {
+        humanScore++;
+        console.log("You WIN! Rock beats scissors");
+      } else if (computerChoice === "paper") {
+        computerScore++;
+        console.log("You LOSE! Paper beats rock");
+      }
+    } else if (humanChoice === "scissors") {
+      if (computerChoice === "paper") {
+        humanScore++;
+        console.log("You WIN! Scissors beats paper");
+      } else if (computerChoice === "rock") {
+        computerScore++;
+        console.log("You LOSE! Rock beats scissors");
+      }
+    } else if (humanChoice === "paper") {
+      if (computerChoice === "rock") {
+        humanScore++;
+        console.log("You WIN! Paper beats rock");
+      } else if (computerChoice === "scissors") {
+        computerScore++;
+        console.log("You LOSE! Scissors beats paper");
+      }
+    } else if (humanChoice === computerChoice) {
+      console.log("DRAW");
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
+
+  if (humanScore > computerScore) {
+    console.log("Human won!");
+  } else if (humanScore < computerScore) {
+    console.log("Computer won!");
+  } else {
+    console.log("Draw! Nobody won!");
+  }
+}
+
+playGame();
