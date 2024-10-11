fa834381585491c8d49ce9f2dd5ed5df170a57a2
RikiReal <rijad@tutanota.de>
2024-10-11 15:53:34 +0200

    Implement getHumanChoice() function
    Add a function that asks the user to pick either rock, paper or scissors

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const validChoices = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-    // Math.random * 3 ==> 0 - 2.99999, 
-    // Math.floor ==> 0 - 2
-    // +1 => 1 - 3
-    const choices = ["rock", "paper", "scissors"];
-    const choiceIndex = Math.floor(Math.random() * 3);
-    return choices[choiceIndex];
+  // Math.random * 3 ==> 0 - 2.99999,
+  // Math.floor ==> 0 - 2
+  const choiceIndex = Math.floor(Math.random() * 3);
+  return validChoices[choiceIndex];
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+  let invalidChoice = true;
+  while (invalidChoice) {
+    let choice = prompt("What's your choice: ");
+    choice = choice.toLowerCase();
+    if (validChoices.includes(choice)) {
+      return choice;
+    }
+    console.log("Invalid choice: " + choice);
+  }
+}
+
+getHumanChoice();
